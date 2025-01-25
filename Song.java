065e6b95b1c94f6e2d1a346374ca47edf4e3cb24
Noah-Posin <nposin26@students.polytechnic.org>
2025-01-25 14:53:32 -0800

    Noah's commit of song

--- Song.java
+++ Song.java
@@ -1,4 +1,7 @@
 package Playlist;
+
+import java.util.Scanner;
+
 /**
  * File for a Song class to be used in the Playlist Project
  * @author
@@ -7,7 +10,10 @@ package Playlist;
 public class Song {
     //Fields-- what information do we want each Song to store?
 
-
+    private String name;
+    private String artist;
+    private String length;
+    private boolean liked;
 
 
     /**
@@ -17,7 +23,13 @@ public class Song {
 
 
 
+     public  Song(String myname, String myartist, int mymin, int mysec){
 
+        name=myname;
+        artist=myartist;
+        length = mymin + ":" + mysec;
+        liked= false;
+    }
 
      /**
       * Methods-- what will you want each Song to do?
@@ -27,5 +39,30 @@ public class Song {
       * What kind of mutator (setter) methods will you need?
       */
 
-
+public String getName(){
+    return name;
 }
+public String getArtist(){
+    return artist;
+}
+public String getTime(){
+    return length;
+}
+public boolean getLike(){
+    return liked;
+}
+public void setLike(){
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Do you want to like this song? y/n");
+    String y = scanner.nextLine();
+    if(y.equals("y")){
+        liked = true;
+    }else{
+        liked=false;
+    }
+}
+public String toString(){
+    return "\"" + name + "\"" + " by " + artist + "("+ length+")";
+}
+}
+
