dcd009ef9cd2826f0db49162cdf051d6c85e2cb7
Ian Teigen <itiegen26@students.polytechnic.org>
2025-01-27 09:18:25 -0800

    Ian's commit of Playlist Tester

--- Playlist.java
+++ Playlist.java
@@ -41,12 +41,12 @@ public class Playlist {
       }
       public void LikeSong(Song b){
         int index = -1;
-        for(int i = 0; i <A.length; i++){
+        for(int i = 0; i <A.size(); i++){
             if (A.get(i).equals(a)){
                 index = i;
 
             }
         }
-        A.get(i).setLike();
+        A.get(index).setLike();
       }
 }
